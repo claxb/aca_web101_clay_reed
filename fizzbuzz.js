4a27a6069449682ecb2a3b7fddf0b995006b9b58
claxb <claxbrown@gmail.com>
2021-01-18 20:25:13 -0600

    push1

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+// list of 1- 1000
+// if number is % by 3, print 'fizz'
+// if number is % by 5, print 'buzz'
+// if number is % by 3 and 5, print 'fizzbuzz'
+
+for (var i=1; i < 101; i++){
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
+
+const scan = () => {
+  for (i=0; i <= 1000; i++){
+  if (i % 3 === 0 && 1 % 5 === 0) console.log('fizzbuzz');
+  else if (i % 3==0) console.log(fizz);
+
+}git checkout MediaDeviceInfo
